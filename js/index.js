fb86c3e35f5b681feef849de1c175d12ba4634fa
SuperDev <SuperDev@doujaz.ca>
2020-03-22 22:47:51 -0700

    Updated table

--- js/index.js
+++ js/index.js
@@ -4,23 +4,66 @@ class Result {
         this.height = height;
         this.firstWeight = firstWeight;
         this.actualWeight = actualWeight;
+
+        let difference = parseInt(actualWeight) - parseInt(firstWeight);
+
+        this.difference = Number.isInteger(difference) ? difference : undefined;
       }
 }
 
 
-let url = "https://spreadsheets.google.com/feeds/list/1bfJxi5SWZKQRYkgWZdyHMOsoOpvbca58A0O4DMYd_uM/od6/public/values?alt=json";
+let url1 = "https://spreadsheets.google.com/feeds/list/1bfJxi5SWZKQRYkgWZdyHMOsoOpvbca58A0O4DMYd_uM/od6/public/values?alt=json";
+let url2 = "https://spreadsheets.google.com/feeds/list/1DDOCDHdb9__khZyobtM6cGOb7YqZ-qEENxw-VoOVSK8/1/public/values?alt=json";
 let results = [];
 
-$.getJSON(url, function(data) {
-    $.each(data.feed.entry, function(index, entry) {
-        let name = entry['gsx$الإسم']['$t'];
-        let height = entry['gsx$الطولبcm']['$t'];
-        let firstWeight = entry['gsx$الوزنبkg']['$t'];
-        let result = new Result(name, height, firstWeight);
-        results.push(result);
-
-        let markup = "<tr><th scope='row'>" + result.name + "</th><td>" + result.height + "</td><td>" + result.firstWeight + "</td><td></td><td></td></tr>";
-        
-        $("table tbody").append(markup);
-    });
+$.getJSON(url1, function(data1) {
+    $.getJSON(url2, function(data2) {
+        $.each(data1.feed.entry, function(index, entry) {
+            let name = entry['gsx$الإسم']['$t'];
+            let height = entry['gsx$الطولبcm']['$t'];
+            let firstWeight = entry['gsx$الوزنبkg']['$t'];
+            let actualWeight = getActualWeight(name, data2);
+            let result = new Result(name, height, firstWeight, actualWeight);
+            results.push(result);
+        });
+    
+        $.each(results, function(index, result) {
+            let markup = "<tr><th scope='row'>" 
+                            + result.name + 
+                            "</th><td>" 
+                            + result.height + 
+                            "</td><td>" 
+                            + result.firstWeight + 
+                            "</td><td>" 
+                            + (result.actualWeight ? result.actualWeight : '') + 
+                            "</td><td>"
+                            + (result.difference ? result.difference : '') + getIcon(result.difference) +
+                            "</td></tr>";
+            
+            $("table tbody").append(markup);
+        }); 
+    });    
 });
+
+function getActualWeight(name, data) {
+    let value;
+    $.each(data.feed.entry, function(index, entry) {
+        if (entry['gsx$الإسم']['$t'] === name) {
+            value = entry['gsx$الوزنبkg']['$t'];
+            return false;
+        }
+    });
+
+    return value;
+}
+
+function getIcon(difference) {
+    if (difference > 0) {
+        return "&nbsp;<img src='img/up.png' alt='Up'>";
+    }
+    else if(difference < 0) {
+        return "&nbsp;<img src='img/down.png' alt='Down'>";
+    }
+    
+    return "";
+}
