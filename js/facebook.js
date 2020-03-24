b2351e3df51c1ec18b879a98db983cf3a8c345fc
SuperDev <SuperDev@doujaz.ca>
2020-03-23 19:46:17 -0700

    Updated Facebook

--- js/facebook.js
+++ js/facebook.js
@@ -1,42 +1,39 @@
 function statusChangeCallback(response) {
-    if (response.status === 'connected') {
-        console.log(response);
-      } else {
-        console.log('not logged');
+  console.log('statusChangeCallback');
+  console.log(response);
+  if (response.status === 'connected') {
+    console.log('logged');
+  } else {
+    console.log('unable to log');
+  }
+}
 
-        FB.login(function(response) {
-          if (response.status === 'connected') {
-            // Logged into your webpage and Facebook.
-            console.log('logged');
-          } else {
-            // The person is not logged into your webpage or we are unable to tell. 
-            console.log('unable to log');
-          }
-        }, {scope: 'public_profile,email'});
-      }
+function checkLoginState() {
+  FB.getLoginStatus(function (response) {
+    statusChangeCallback(response);
+  });
 }
 
 
+window.fbAsyncInit = function () {
+  FB.init({
+    appId: '645069032992683',
+    cookie: true,
+    xfbml: true,
+    version: 'v6.0'
+  });
 
-window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '645069032992683',
-      cookie     : true,
-      xfbml      : true,
-      version    : 'v6.0'
-    });
-      
-    FB.AppEvents.logPageView();
-    
-    FB.getLoginStatus(function(response) {
-        statusChangeCallback(response);
-    });
-  };
+  FB.AppEvents.logPageView();
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
+  FB.getLoginStatus(function (response) {
+    statusChangeCallback(response);
+  });
+};
+
+(function (d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) { return; }
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
